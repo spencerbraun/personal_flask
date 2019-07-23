98f9dc7af27ed68f56ba52f9a32865ddf800e026
Spencer Braun <spencer.braun@gmail.com>
2019-07-23 10:00:55 -0400

    fix trailing slash css rendering issue

--- app2.py
+++ app2.py
@@ -20,54 +20,60 @@ from flask_frozen import Freezer
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
+FLATPAGES_EXTENSION = ".md"
 FREEZER_REMOVE_EXTRA_FILES = False
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
-FLATPAGES_EXTENSION_CONFIG = {
-    'codehilite': {
-        'linenums': 'True'
-    }
-}
-FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
+FLATPAGES_MARKDOWN_EXTENSIONS = ["codehilite"]
+FLATPAGES_EXTENSION_CONFIG = {"codehilite": {"linenums": "True"}}
+FLATPAGES_ROOT = os.path.join(APP_DIR, "pages")
 
 app = Flask(__name__)
+app.config.from_pyfile("settings.py")
 app.url_map.strict_slashes = False
-# app.config.from_pyfile('settings.py')
-app.config.from_object(__name__)
+# app.config.from_object(__name__)
 pages = FlatPages(app)
-# freezer = Freezer(app)
+freezer = Freezer(app)
 
-@app.before_request
-def clear_trailing():
-    from flask import redirect, request
+#
+# @app.before_request
+# def clear_trailing():
+#     from flask import redirect, request
+#
+#     rp = request.path
+#     if rp != "/" and rp.endswith("/"):
+#         return redirect(rp[:-1])
 
-    rp = request.path
-    if rp != '/' and rp.endswith('/'):
-        return redirect(rp[:-1])
 
-# @freezer.register_generator
-# def page_details():
-#     for path in pages.iter():
-#         yield {'path': path}
-
-@app.route('/')
+@app.route("/")
 def index():
-    posts = [page for page in pages if 'date' in page.meta]
-    sorted_posts = sorted(posts, reverse=True,
-        key=lambda page: page.meta['date'])
-    return render_template('index.html', pages=pages)
+    posts = [page for page in pages if "date" in page.meta]
+    sorted_posts = sorted(posts, reverse=True, key=lambda page: page.meta["date"])
+    return render_template("index.html", pages=sorted_posts)
 
-@app.route('/<path:path>/')
+
+@app.route("/about/")
+def about():
+    # about = [page for page in pages if "About" == page.meta['title']]
+    # return render_template("page.html", page=about)
+    return render_template("about.html")
+
+
+@app.route("/writing/")
+def writing():
+    posts = [
+        page for page in pages if all([tag in page.meta for tag in ["date", "writing"]])
+    ]
+    sorted_posts = sorted(posts, reverse=True, key=lambda page: page.meta["date"])
+    return render_template("writing.html", pages=sorted_posts)
+
+
+@app.route("/<path:path>/")
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('page.html', page=page)
-
+    return render_template("page.html", page=page)
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5000))
-    #
-    # if len(sys.argv) > 1 and sys.argv[1] == "build":
-    #     freezer.freeze()
-    # else:
-    app.run(host='localhost', port=8000)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(host="localhost", port=8000)
--- settings.py
+++ settings.py
@@ -1,25 +1,16 @@
-#!/usr/bin/env python
-
 import os
 
-REPO_NAME = "personal_flask"  # Used for FREEZER_BASE_URL
-DEBUG = True
+REPO_NAME = "personal_flask"
 
-# Assumes the app is located in the same directory
-# where this file resides
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
-
-def parent_dir(path):
-    '''Return the parent of a directory.'''
-    return os.path.abspath(os.path.join(path, os.pardir))
-
-FREEZER_DESTINATION = APP_DIR
-# Since this is a repo page (not a Github user page),
-# we need to set the BASE_URL to the correct url as per GH Pages' standards
-FREEZER_BASE_URL = f"http://localhost/"
-FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
-                                    # will be deleted when you run the freezer
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
-FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
-FLATPAGES_EXTENSION = '.md'
+DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+FREEZER_REMOVE_EXTRA_FILES = False
+FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
+FLATPAGES_EXTENSION_CONFIG = {
+    'codehilite': {
+        'linenums': 'True'
+    }
+}
+FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
