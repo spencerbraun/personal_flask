e0bcd3fdae7c14e778f0f179d106d9c9e2f6c6fa
Spencer Braun <spencer.braun@gmail.com>
2019-07-23 10:15:08 -0400

    rename app.py, clean up settings

--- app.py
+++ app.py
@@ -19,30 +19,10 @@ from flask import (
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-DEBUG = True
-FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = ".md"
-FREEZER_REMOVE_EXTRA_FILES = False
-FLATPAGES_MARKDOWN_EXTENSIONS = ["codehilite"]
-FLATPAGES_EXTENSION_CONFIG = {"codehilite": {"linenums": "True"}}
-FLATPAGES_ROOT = os.path.join(APP_DIR, "pages")
-
 app = Flask(__name__)
 app.config.from_pyfile("settings.py")
-app.url_map.strict_slashes = False
-# app.config.from_object(__name__)
 pages = FlatPages(app)
 freezer = Freezer(app)
-
-#
-# @app.before_request
-# def clear_trailing():
-#     from flask import redirect, request
-#
-#     rp = request.path
-#     if rp != "/" and rp.endswith("/"):
-#         return redirect(rp[:-1])
 
 
 @app.route("/")
@@ -54,8 +34,6 @@ def index():
 
 @app.route("/about/")
 def about():
-    # about = [page for page in pages if "About" == page.meta['title']]
-    # return render_template("page.html", page=about)
     return render_template("about.html")
 
 
@@ -77,21 +55,21 @@ def page(path):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--build',
-        action='store_true',
-        help='Freezes flask application into build dir'
+        "--build",
+        action="store_true", 
+        help="Freezes flask application into build dir"
     )
     parser.add_argument(
-        '--build_live',
-        action='store_true',
-        help='Freezes flask application and build to docs dir'
+        "--build_live",
+        action="store_true",
+        help="Freezes flask application and build to docs dir",
     )
     args = parser.parse_args()
 
     if args.build or args.build_live:
         freezer.freeze()
         if args.build_live:
-            shutil.copytree('build', 'docs')
+            shutil.copytree("build", "docs")
     else:
         app.run(host="localhost", port=8000)
 
